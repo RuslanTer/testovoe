07fd3554452c9d7bba6da4b39a8a71eb855e4383
Ruslan Terehov <terehov.ruslan.dev18@gmail.com>
2020-09-16 17:14:23 +0300

    fix Caesar

--- neponyatno.kt
+++ neponyatno.kt
@@ -41,7 +41,8 @@ fun сaesarEncryption(text: String, rot: Int, alpha: String):String {
             continue
         }
         var y = alpha.indexOf(symbol)
-        var x = kotlin.math.abs(y + rot) % n
+        var x = kotlin.math.abs(y + n-rot%n) % n
+
         result += alpha[x]
     }
     return result
